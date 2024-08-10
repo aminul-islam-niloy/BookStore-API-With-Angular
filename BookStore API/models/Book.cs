55ec4c3a6ec8af93ea0c5a48be78f653f718129d
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-08-10 16:59:31 +0600

    Create Book Models

---
+++ BookStore API/models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace BookStore_API.models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public string Genre { get; set; }
+    }
+}
