10037212c329bf37ecdd62491e9bf5e9019387dd
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-08-10 17:03:48 +0600

    Database Configuration

---
+++ BookStore API/Data/BookContext.cs
@@ -0,0 +1,12 @@
+﻿using BookStore_API.models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore_API.Data
+{
+    public class BookContext:DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
--- BookStore API/Program.cs
+++ BookStore API/Program.cs
@@ -1,11 +1,19 @@
+using BookStore_API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configure EF Core with SQL Server
+builder.Services.AddDbContext<BookContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
